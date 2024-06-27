8c391b66f6d37f04dad98bfef3e880d3e1500e34
Abiemi-ux <abiemib@gmail.com>
2024-06-27 22:04:13 +0300

    Added backend

---
+++ assets/js/server.js
@@ -0,0 +1,41 @@
+// server.js
+
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/send-email', (req, res) => {
+    const { name, email, project, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'itsabiemi@gmail.com',
+            pass: '?Abiemi_19019.'
+        }
+    });
+
+    const mailOptions = {
+        from: email,
+        to: 'itsabiemi@gmail.com',
+        subject: `Contact form submission from ${name}`,
+        text: `Name: ${name}\nEmail: ${email}\nProject: ${project}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+        res.status(200).send('Message sent: ' + info.response);
+    });
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
