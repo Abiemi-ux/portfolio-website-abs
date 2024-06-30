c863c1f6822929d0c24dfebb26b702b2f6249329
Abiemi-ux <abiemib@gmail.com>
2024-06-30 10:33:50 +0300

    changed skill section

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 // server.js
 
 const express = require('express');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+const allowedOrigins = ['http://localhost:5500', 'http://127.0.0.1:5500'];
+
 app.use(cors({
-    origin: 'https://portfolio-abs.netlify.app',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
+    origin: function (origin, callback) {
+        // allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true // Add this if you need to send cookies or HTTP authentication headers along with the request
 }));
 
 app.post('/send-email', (req, res) => {
@@ -21,22 +30,24 @@ app.post('/send-email', (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'abiemib@gmail.com',
-            pass: 'cxgl kstd dzgu kjpd'
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
     });
 
     const mailOptions = {
         from: email,
-        to: 'itsabiemi@gmail.com',
+        to: process.env.EMAIL,
         subject: `Contact form submission from ${name}`,
         text: `Name: ${name}\nEmail: ${email}\nProject: ${project}\nMessage: ${message}`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
+            console.error('Error sending email:', error);
             return res.status(500).send(error.toString());
         }
+        console.log('Email sent:', info.response);
         res.status(200).send('Message sent: ' + info.response);
     });
 });
